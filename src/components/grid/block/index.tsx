7d3f2fdb1ed303cd8ea0b962140fe6bf95ecfa86
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-10-26 09:28:13 +0000

    added redux store, action, action creater and reducer

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,4 +1,8 @@
 import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
+
+import { IReducer } from 'reducers';
+import { SudokuInteger } from 'typings';
 
 import { Container } from './styles';
 
@@ -7,8 +11,19 @@ interface IProps {
   rowIndex: number;
 }
 
+interface IState {
+  value: SudokuInteger;
+}
+
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
-  return <Container data-cy={`block-${rowIndex}-${colIndex}`}>0</Container>;
+  const state = useSelector<IReducer, IState>(({ grid }) => ({
+    value: grid ? grid[rowIndex][colIndex] : 0,
+  }));
+  return (
+    <Container data-cy={`block-${rowIndex}-${colIndex}`}>
+      {state.value}
+    </Container>
+  );
 };
 
 export default Block;
--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,9 +1,20 @@
-import React, { FC, Children } from 'react';
+import React, { FC, Children, useCallback, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { AnyAction, Dispatch } from 'redux';
+
+import { createGrid } from 'reducers';
 
 import Block from './block';
 import { Container, Row } from './styles';
 
 const Grid: FC = () => {
+  const dispatch = useDispatch<Dispatch<AnyAction>>();
+  const create = useCallback(() => dispatch(createGrid()), [dispatch]);
+
+  useEffect(() => {
+    create();
+  }, [create]);
+
   return (
     <Container data-cy="grid-container">
       {Children.toArray(
